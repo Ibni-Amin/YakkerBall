7c5ae02af8118ddf6c18ea275036c1f3e7343d3b
Amin <amin@4iisolutions.com>
2024-03-21 23:02:55 +0500

    New build changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'yakkarball',
+  appName: 'yakkerball',
   webDir: 'www',
   server: {
     androidScheme: 'https',
